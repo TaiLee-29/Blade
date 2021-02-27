1a600a26796124e7daaaf895d8da4eb9c5c66c34
TaiLee-29 <dreih796@gmail.com>
2021-02-27 21:21:55 +0200

    Pagination

--- config/eloquent.php
+++ config/eloquent.php
@@ -5,14 +5,22 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
+    /*'driver'    => 'mysql',
     'host'      => '127.0.0.1',
     'database'  => 'eloquent',
     'username'  => 'root',
     'password'  => 'secret',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
+    'prefix'    => '',*/
+    'driver' => getenv('DATABASE_DRIVER'),
+    'host' => getenv('DATABASE_HOST'),
+    'database' => getenv('DATABASE_DB'),
+    'username' => getenv('DATABASE_USERNAME'),
+    'password' => getenv('DATABASE_PASSWORD'),
+    'charset' => getenv('DATABASE_CHARSET'),
+    'collation' => getenv('DATABASE_COLLATION'),
+    'prefix' => getenv('DATABASE_PREFIX'),
 ]);
 
 // Set the event dispatcher used by Eloquent models... (optional)
